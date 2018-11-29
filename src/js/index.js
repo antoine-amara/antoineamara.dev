64ec9b4b8ce70554860fd91bdf399decdc709cc3
Antoine AMARA <amara.antoine@gmail.com>
2018-11-30 00:17:36 +0100

    feat: design the contact section
    Add html structure. Design input form.
    Design textarea.
    Create a custom select input.
    Create a custom button.
    
    create the structure for contact and add basics rules for inputs.
    
    style the select and update input to be ok. reset outline for each input and select.
    
    add button to contact form.

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,6 @@ import './menu'
 console.info('**********************************************')
 console.info('* Welcome to ColorSpace Developer Portfolio *')
 console.info('**********************************************')
+
+const contactSubmitButton = document.getElementsByClassName('button')[0]
+contactSubmitButton.addEventListener('click', e => e.preventDefault())
