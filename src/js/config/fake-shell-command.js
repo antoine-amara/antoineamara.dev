1a58a76c301148c76f593c17d79c042db95b07cb
Antoine AMARA <amara.antoine@gmail.com>
2021-01-24 19:28:34 +0100

    feat: add the configuration for fake terminal
    add the json configuration with all commands for the fake terminal in
    the about me section.
    these commands describe all competences for each domain (web, ops, ...)

---
+++ src/js/config/fake-shell-command.js
@@ -0,0 +1,74 @@
+export default {
+  main: `Name: Antoine AMARA.
+  Job: Javascript Fullstack Developper.
+  Stack:
+  \t\t* React / Redux
+  \t\t* NodeJS
+  \t\t* GraphQL
+  \t\t* SQL
+  \t\t* Jest
+  \t\t* Webpack
+  \t\t* Docker for dev
+  type 'antoine --help' for more information about technologies I know.
+  `,
+  'antoine --help': `See my competences by theme:
+  \t\t* antoine stack --web: see the list of web frameworks I used.
+  \t\t* antoine stack --ops: see the list of knowledge about ops (system, deployment framework, CI).
+  \t\t* antoine stack --tools: see the development tools I used (for web development).
+  \t\t* antoine stack languages: see the languages I used when I develop.
+  \t\t* antoine stack manage: see principle I know to construct good software (project management, project structuration).
+  `,
+  'antoine stack --web': `1:basic -- 3: good knowledge -- 5: mastering it
+  ------------------
+  React:\t\t\t[#####]
+  Redux:\t\t\t[#####]
+  NodeJS:\t\t[#####]
+  GraphQL:\t[####\t]
+  Express:\t[#####]
+  Angular:\t[##\t\t\t]
+  Symfony:\t[##\t\t\t]
+  Jest:\t\t\t\t[###\t\t]
+  SQL:\t\t\t\t\t[####\t]
+  MongoDB:\t[####\t]
+  ------------------
+  `,
+  'antoine stack --ops': `1:basic -- 3: good knowledge -- 5: mastering it
+  -------------------------
+  Linux:\t\t\t\t\t\t\t\t\t\t[###\t\t]
+  Docker:\t\t\t\t\t\t\t\t\t[####\t]
+  Docker-Compose:\t[###\t\t]
+  Kubernetes:\t\t\t\t\t[#\t\t\t\t]
+  GitlabCI:\t\t\t\t\t\t\t[##\t\t\t]
+  Terraform:\t\t\t\t\t\t[##\t\t\t]
+  -------------------------
+  `,
+  'antoine stack --tools': `1:basic -- 3: good knowledge -- 5: mastering it
+  ------------------------
+  Webpack:\t\t\t\t\t\t\t[####\t]
+  Eslint:\t\t\t\t\t\t\t\t[####\t]
+  Babel:\t\t\t\t\t\t\t\t\t[###\t\t]
+  Markdown:\t\t\t\t\t\t[#####]
+  Editor Config:\t[###\t\t]
+  ------------------------
+  `,
+  'antoine languages': `1:basic -- 3: good knowledge -- 5: mastering it
+  ---------------------
+  Javascript:\t[#####]
+  PHP:\t\t\t\t\t\t\t\t[###\t\t]
+  Python:\t\t\t\t\t[###\t\t]
+  Java:\t\t\t\t\t\t\t[###\t\t]
+  C:\t\t\t\t\t\t\t\t\t\t[###\t\t]
+  C++:\t\t\t\t\t\t\t\t[###\t\t]
+  Go:\t\t\t\t\t\t\t\t\t[#\t\t\t\t]
+  ---------------------
+  `,
+  'antoine manage': `1:basic -- 3: good knowledge -- 5: mastering it
+  ----------------------------------------
+  Scrum:\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[###\t\t]
+  Good Practices:\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[####\t]
+  Lead/Architect React Project:\t\t[###\t\t]
+  Lead/Architect NodeJS Project:\t[###\t\t]
+  Kaban:\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t[##\t\t\t]
+  ----------------------------------------
+  `
+}
